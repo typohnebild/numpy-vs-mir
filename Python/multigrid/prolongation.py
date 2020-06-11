20e74d6e6022f5fb295d7bd65e60b35ea7924f72
Christoph Alt <christoph.alt@fau.de>
2020-06-11 17:19:00 +0200

    comments on prolongagation

--- Python/multigrid/prolongation.py
+++ Python/multigrid/prolongation.py
@@ -2,13 +2,18 @@ import numpy as np
 
 
 def prolongation(e, fine_shape):
+    """
+        This interpolates/ prolongates to a grid of fine_shape
+        @param e
+        @param fine_shape targeted shape
+        @return grid with fine_shape
+    """
     alpha = len(e.shape)
     w = np.zeros(fine_shape)
     end = e.shape[0] - (w.shape[0] + 1) % 2
     wend = w.shape[0] - (w.shape[0] + 1) % 2
 
     if alpha == 1:
-        # w[0] = e[0] / 2
         w[:-1:2] = e[:-1]
         w[1:-1:2] = (e[:end - 1] + e[1:end]) / 2
         w[-1] = e[-1]
@@ -27,6 +32,7 @@ def prolongation(e, fine_shape):
         w[1:-1:2, :-1:2] = (e[:end - 1, :-1] + e[1:end, :-1]) / 2
         w[1:-1:2, -1] = (e[:end - 1, -1] + e[1:end, -1]) / 2
 
+        # average of 4 neighbors
         w[1:-1:2, 1:-1:2] = (w[2:wend:2, 1:wend:2] +
                              w[:wend - 1:2, 1:wend:2] +
                              w[1:wend:2, :wend - 1:2] +
