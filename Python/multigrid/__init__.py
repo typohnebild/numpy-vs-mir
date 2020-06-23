c367b89c1d12a2155f19c268e81b43c627c050e5
bu49mebu <tobias.ts.schmidt@fau.de>
2020-06-23 18:15:26 +0200

    set border of residual to 0 in poisson multigrid

--- Python/multigrid/__init__.py
+++ Python/multigrid/__init__.py
@@ -5,6 +5,7 @@ import numpy as np
 from ..GaussSeidel.GaussSeidel_RB import GS_RB
 from ..GaussSeidel.GaussSeidel import gauss_seidel
 from ..tools.operators import poisson_operator_like
+# from ..tools.util import draw2D
 
 from .restriction import restriction
 from .prolongation import prolongation
@@ -22,11 +23,10 @@ def poisson_multigrid(F, U, l, v1, v2, mu):
        @param mu iterations for recursive call
        @return x n vector
     """
-
     # abfangen, dass Level zu gross wird
     if l <= 1 or U.shape[0] <= 1:
         # solve
-        return GS_RB(F, U=U, max_iter=1000)
+        return GS_RB(F, U=U, max_iter=10000)
 
     # smoothing
     U = GS_RB(F, U=U, max_iter=v1)
@@ -34,6 +34,8 @@ def poisson_multigrid(F, U, l, v1, v2, mu):
     r = F - apply_poisson(U)
     # restriction
     r = restriction(r)
+    # do not update border
+    r[0, :] = r[:, 0] = r[-1, :] = r[:, -1] = 0
 
     # recursive call
     e = np.zeros_like(r)
@@ -42,11 +44,12 @@ def poisson_multigrid(F, U, l, v1, v2, mu):
     # prolongation
     e = prolongation(e, U.shape)
 
-    # do not update border
-    e[0, :] = e[:, 0] = e[-1, :] = e[:, -1] = 0
+    # temp
+    # draw2D(e)
 
     # correction
     U = U + e
+
     # post smoothing
     return GS_RB(F, U=U, max_iter=v2)
 
@@ -66,7 +69,8 @@ def general_multigrid(A, F, U, l, v1, v2, mu):
     # abfangen, dass Level zu gross wird
     if l <= 1 or U.shape[0] <= 1:
         # solve
-        return gauss_seidel(A, F, U, max_iter=1000)
+        # return gauss_seidel(A, F, U, max_iter=100000)
+        return np.linalg.solve(A, F)
 
     # smoothing
     U = gauss_seidel(A, F, U, max_iter=v1)
@@ -88,7 +92,7 @@ def general_multigrid(A, F, U, l, v1, v2, mu):
             v1,
             v2,
             mu)
-    print(np.linalg.norm(e), np.linalg.norm(r))
+    # print(np.linalg.norm(e), np.linalg.norm(r))
     # prolongation
     e = prolongation(e, U.shape)
 
@@ -97,5 +101,7 @@ def general_multigrid(A, F, U, l, v1, v2, mu):
 
     # correction
     U = U + e
+
     # post smoothing
     return gauss_seidel(A, F, U, max_iter=v2)
+
