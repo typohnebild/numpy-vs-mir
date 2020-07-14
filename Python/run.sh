83a696d33069a4e8b3160f286226dfffa233060f
Christoph Alt <christoph.alt@fau.de>
2020-07-14 20:38:57 +0200

    somehow a big problem

--- Python/run.sh
+++ Python/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
-OUTFILE=outfile
-N=256
-for _ in $(seq 6)
+OUTFILE="outfile_$(hostname)"
+N=128
+for _ in $(seq 10)
 do
     x=$(perf stat -M GFLOPS ./measure.py $N 2>&1 | grep -i 'fp\|elapsed' | awk '{ print $1}' | tr '\n' ':')
     printf "%b:%b\\n" "$N" "$x" >> $OUTFILE
