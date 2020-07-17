f08f2bc3042b398463e8eed74bfec2f84e8e7071
Christoph Alt <christoph.alt@fau.de>
2020-07-17 10:29:49 +0200

    more measurements

--- Python/run.sh
+++ Python/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 OUTFILE="outfile_$(hostname)"
-N=128
-for _ in $(seq 10)
+N=1000
+for _ in $(seq 12)
 do
     x=$(perf stat -M GFLOPS ./measure.py $N 2>&1 | grep -i 'fp\|elapsed' | awk '{ print $1}' | tr '\n' ':')
-    printf "%b:%b\\n" "$N" "$x" >> $OUTFILE
-    N=$((N * 2))
+    printf "%b:%b\\n" "$N" "$x" >> "$OUTFILE"
+    N=$((N + 1000))
 done
