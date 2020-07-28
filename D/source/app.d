2fe53dcc984b6b1d3c30750b9030fbfb54bc7ece
bu49mebu <tobias.ts.schmidt@fau.de>
2020-07-28 18:30:32 +0200

    started Gauss Seidel Red Black in D

--- D/source/app.d
+++ D/source/app.d
@@ -1,9 +1,21 @@
-import std.range, std.stdio;
+import std.stdio;
+import std.range: generate;
+import std.random: uniform;
+import std.array;
+import std.algorithm;
+import mir.ndslice;
+
+import multid.gaussseidel.redblack;
+
 
 void main()
 {
-    auto sum = 30.0;
-    auto count = 50;//stdin.byLine.tee!(l => sum += l.length).walkLength;
-
-    write("Average line length: ", count ? sum / count : 0);
+    auto F = slice!double(5, 2);
+    auto fun = generate!(() => uniform(0.0, 1.0));
+    F.field.fill(fun);
+    auto U = slice!double([5,2], 0.0);
+    double h = 1;
+    // auto F = generate!(() => uniform(0, 0.99)).take(10).array.sliced;
+    // F = F.reshape([5,2]);
+    writeln(GS_RB!(double, 2, 666)(F, U, h));
 }
---
+++ D/source/multid/gaussseidel/redblack.d
@@ -0,0 +1,80 @@
+module multid.gaussseidel.redblack;
+
+import std.stdio;
+import mir.ndslice;
+
+enum color {red = 0, black = 1};
+
+/++
+This is a Gauss Seidel Red Black implementation for 1D
++/
+Slice!(T*, Dim) GS_RB(
+    T,
+    size_t Dim,
+    size_t max_iter = 10_000_000,
+    size_t norm_iter = 10_000,
+    T eps = 1e-8)
+    (Slice!(T*, Dim) F, Slice!(T*, Dim) U, double h)
+{
+    static assert(Dim >=1 && Dim <= 3);
+
+    double h2 = h * h;
+
+    // if (Dim == 1)
+    // {
+    //     // TODO
+    // }
+    // else if (Dim == 2)
+    // {
+    //     // TODO
+    // }
+    // else if (Dim == 3)
+    // {
+    //     // TODO
+    // }
+
+    foreach(it; 0..max_iter)
+    {
+        if(it % norm_iter == 0)
+        {
+            //TODO: implemenent apply_poisson
+            // r = F - apply_poisson(U, h)
+            // ...
+        }
+        // rote Halbiteration
+        sweep(Dim) (color.red, F, U, h2)
+        // schwarze Halbiteration
+        sweep(Dim) (color.black, F, U, h2)
+    }
+
+    return U;
+}
+
+/++
+This is a sweep implementation for 1D
++/
+void sweep(size_t Dim: 1) (int color, auto F, auto U, double h2)
+{
+    int err;
+    size_t n = F.shape(err);
+    assert(err == 0);
+
+
+}
+
+/++
+This is a sweep implementation for 2D
++/
+void sweep(size_t Dim: 2) (int color, auto F, auto U, double h2)
+{
+
+}
+
+/++
+This is a sweep implementation for 3D
++/
+void sweep(size_t Dim: 3) (int color, auto F, auto U, double h2)
+{
+
+}
+
