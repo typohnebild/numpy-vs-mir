249e90a67a5a2ef4f86a54397bd62e84cacbf194
Christoph Alt <christoph.alt@fau.de>
2020-07-29 18:53:11 +0200

    reworked the run script

--- Python/run.sh
+++ Python/run.sh
@@ -1,9 +1,37 @@
 #!/usr/bin/sh
 OUTFILE="outfile_$(hostname)_$(date +%d%m)"
+
+get_infos(){
+
+    echo "############ INFOS"
+    date
+    lscpu | grep -i 'model name' | awk -F: '{ print $2}' | sed -e 's/^\s*//g'
+    python -c 'import numpy; print("numpy version:", numpy.__version__); numpy.show_config()'
+    echo "############ END INFOS"
+}
+
+[ -e "$OUTFILE" ] || get_infos >> "$OUTFILE" || exit 1
+
+
 N=1000
-for _ in $(seq 12)
-do
-    x=$(perf stat -M GFLOPS ./measure.py $N 2>&1 | grep -i 'fp\|elapsed' | awk '{ print $1}' | tr '\n' ':')
-    printf "%b:%b\\n" "$N" "$x" >> "$OUTFILE"
-    N=$((N + 1000))
-done
+paranoid=$(cat /proc/sys/kernel/perf_event_paranoid)
+
+iter=${1:-2}
+if [ "$paranoid" -lt 3 ]  && perf list eventgroups | grep -q FLOPS
+then
+    for _ in $(seq "$iter")
+    do
+        x=$(perf stat -M FLOPS ./measure.py $N 2>&1 | grep -i 'fp\|elapsed' | awk '{ print $1}' | tr '\n' ':')
+        printf "%b:%b\\n" "$N" "$x" >> "$OUTFILE"
+        N=$((N + 1000))
+    done
+else
+    for _ in $(seq "$iter")
+    do
+        x=$(/usr/bin/time -f %e ./measure.py $N 2>&1)
+        printf "%b:%b\\n" "$N" "$x" >> "$OUTFILE"
+        N=$((N + 1000))
+    done
+fi
+
+exit 0
