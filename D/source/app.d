76b4bd7a5cf325ed515297ef6cc7ff25676c141f
Christoph Alt <christoph.alt@fau.de>
2020-07-31 09:58:13 +0200

    more on rbgs in d

--- D/source/app.d
+++ D/source/app.d
@@ -9,10 +9,10 @@ import multid.gaussseidel.redblack;
 
 void main()
 {
-    auto F = slice!double(10, 1);
+    auto U = slice!double(10);
     auto fun = generate!(() => uniform(0.0, 1.0));
-    F.field.fill(fun);
-    auto U = slice!double([10, 1], 0.0);
+    U.field.fill(fun);
+    auto F = slice!double([10], 0.0);
     const double h = 1 / 10;
     // auto F = generate!(() => uniform(0, 0.99)).take(10).array.sliced;
     // F = F.reshape([5,2]);
