8edbff0003dea0ec001e9e37d47fae9c2ff71134
Christoph Alt <christoph.alt@fau.de>
2020-08-19 09:48:23 +0200

    super creative testcases for norm

--- D/source/multid/tools/norm.d
+++ D/source/multid/tools/norm.d
@@ -8,6 +8,12 @@ import mir.ndslice;
 +/
 auto nrmL2(V)(V v)
 {
-
     return v.map!(x => x * x).sum.sqrt;
+}
+
+unittest
+{
+    assert([1, 2, 3, 4].sliced!double.nrmL2 == 30.0.sqrt);
+    assert([1, 1].sliced!double.nrmL2 == 2.0.sqrt);
+    assert([1, 1, 1, 1].sliced!double.nrmL2 == 2.0);
 }
