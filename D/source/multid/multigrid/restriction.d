4bc9f990311b7723ebfbb0996a1e9c506126d246
bu49mebu <tobias.ts.schmidt@fau.de>
2020-09-01 12:27:15 +0200

    implemented weighted_restriction in D

--- D/source/multid/multigrid/restriction.d
+++ D/source/multid/multigrid/restriction.d
@@ -93,28 +93,83 @@ Slice!(T*, Dim) restriction(T, size_t Dim:3)(Slice!(T*, Dim) A)
 }
 
 /++
-This is the implementation of a weighted_restriction
+This is the implementation of a weighted_restriction 1D
 +/
-void weighted_restriction(T, size_t Dim)(Slice!(T*, Dim) A)
+Slice!(T*, Dim) weighted_restriction(T, size_t Dim:1)(Slice!(T*, Dim) A)
 {
-    //TODO
+    const auto M = A.shape[0];
+    const auto N = M / 2 + 1;
+    auto ret = restriction!(T,Dim)(A);
+    auto AF = A.field;
+    foreach (i; 1u .. N - 1u)
+    {
+        ret.field[i] = ret.field[i] / 2 + (AF[i*2 - 1u] + AF[i*2 + 1u]) / cast(T)(4);
+    }
+    return ret;
+}
 
-    static if (Dim == 1)
+/++
+This is the implementation of a weighted_restriction 2D
++/
+Slice!(T*, Dim) weighted_restriction(T, size_t Dim:2)(Slice!(T*, Dim) A)
+{
+    const auto M = A.shape[0];
+    const auto N = M / 2 + 1;
+    auto ret = restriction!(T,Dim)(A);
+    auto AF = A.field;
+
+    foreach (i; 1u .. N-1u)
     {
-        //TODO
+        foreach(j; 1u .. N-1u)
+        {
+            auto indexR = i * N + j;
+            auto indexA = i * M * 2 + j * 2;
+            ret.field[indexR] = ret.field[indexR] / cast(T)(4) +
+                (AF[indexA - 1] + AF[indexA + 1] +
+                 AF[indexA - M] + AF[indexA + M]) / cast(T)(8) +
+                (AF[indexA - 1 - M] + AF[indexA - 1 + M] +
+                 AF[indexA + 1 - M] + AF[indexA + 1 + M]) / cast(T)(16);
+        }
     }
-    else static if (Dim == 2)
+    return ret;
+}
+
+/++
+This is the implementation of a weighted_restriction 3D
++/
+Slice!(T*, Dim) weighted_restriction(T, size_t Dim:3)(Slice!(T*, Dim) A)
+{
+    const auto M = A.shape[0];
+    const auto N = M / 2 + 1;
+    auto ret = restriction!(T,Dim)(A);
+    auto AF = A.field;
+    foreach (k; 1u .. N - 1u)
     {
-        //TODO
+        foreach (i; 1u .. N-1u)
+        {
+            foreach(j; 1u .. N-1u)
+            {
+                auto indexR = k*(N*N) + i * N + j;
+                auto indexA = k*(M*M)*2 + i * M * 2 + j * 2;
+                ret.field[indexR] = (ret.field[indexR] * cast(T)(8) +
+                    (AF[indexA - 1] + AF[indexA + 1] +
+                     AF[indexA - M] + AF[indexA + M] +
+                     AF[indexA - M*M] + AF[indexA + M*M]) * cast(T)(4) +
+                    (AF[indexA - 1 - M] + AF[indexA + 1 + M] +
+                     AF[indexA - 1 + M] + AF[indexA + 1 - M] +
+                     AF[indexA - 1 - M*M] + AF[indexA + 1 + M*M] +
+                     AF[indexA - 1 + M*M] + AF[indexA + 1 - M*M] +
+                     AF[indexA - M - M*M] + AF[indexA + M + M*M] +
+                     AF[indexA - M + M*M] + AF[indexA + M - M*M]) * cast(T)(2) +
+                    (AF[indexA - 1 - M - M*M] + AF[indexA + 1 - M - M*M] +
+                     AF[indexA - 1 + M - M*M] + AF[indexA + 1 + M - M*M] +
+                     AF[indexA - 1 - M + M*M] + AF[indexA + 1 - M + M*M] +
+                     AF[indexA - 1 + M + M*M] + AF[indexA + 1 + M + M*M]) * cast(T)(1)) /
+                    cast(T)(64);
+            }
+        }
     }
-    else static if (Dim == 3)
-    {
-        //TODO
-    }
-    else
-    {
-        static assert(false, Dim.stringof ~ " is not a supported dimension!");
-    }
+    return ret;
 }
 
 
@@ -189,4 +244,116 @@ unittest
         [204., 206., 208., 209.],
         [210., 212., 214., 215.]]];
     assert(ret == correct);
+}
+
+
+// Test weighted_restriction 1D long
+unittest
+{
+    auto arr = iota(10).slice;
+    auto ret = weighted_restriction!(long, 1)(arr);
+    auto correct = [0,2,4,6,8,9];
+    assert (ret == correct);
+
+    arr = iota(11).slice;
+    ret = weighted_restriction!(long, 1)(arr);
+    correct = [0,2,4,6,8,10];
+    assert (ret == correct);
+}
+// Test weighted_restriction 1D double
+unittest
+{
+    auto arr2 = [1.0, 2.0, 3.0, 2.0, 1.0].sliced;
+    auto ret2 = weighted_restriction!(double, 1)(arr2);
+    auto correct2 = [1.0, 2.5, 1.0];
+    assert (ret2 == correct2);
+
+    arr2 = [1.0, 2.0, 3.0, 3.0, 2.0, 1.0].sliced;
+    ret2 = weighted_restriction!(double, 1)(arr2);
+    correct2 = [1.0, 2.75, 2.0, 1.0];
+    assert (ret2 == correct2);
+}
+
+// Test weighted_restriction 2D long
+unittest
+{
+    auto arr = iota([5,5]).slice;
+    auto ret = weighted_restriction!(long, 2)(arr);
+    auto correct = [[0,2,4],[10,12,14],[20,22,24]];
+    assert (ret == correct);
+
+    arr = iota([6,6]).slice;
+    ret = restriction!(long,2)(arr);
+    correct = [[0,2,4,5], [12,14,16,17], [24,26,28,29], [30,32,34,35]];
+    assert (ret == correct);
+}
+// Test weighted_restriction 2D double
+unittest
+{
+    auto arr2 = [1.0, 2.0, 3.0, 2.0, 1.0,
+                 2.0, 3.0, 4.0, 3.0, 2.0,
+                 3.0, 4.0, 5.0, 4.0, 3.0,
+                 4.0, 5.0, 6.0, 5.0, 4.0,
+                 5.0, 6.0, 7.0, 6.0, 5.0].sliced(5,5);
+    auto ret2 = weighted_restriction!(double, 2)(arr2);
+    auto correct2 = [[1.0, 3.0, 1.0],
+                     [3.0, 4.5, 3.0],
+                     [5.0, 7.0, 5.0]];
+    assert (ret2 == correct2);
+
+    arr2 = [1.0, 2.0, 3.0, 3.0, 2.0, 1.0,
+            2.0, 3.0, 4.0, 4.0, 3.0, 2.0,
+            3.0, 4.0, 5.0, 5.0, 4.0, 3.0,
+            4.0, 5.0, 6.0, 6.0, 5.0, 4.0,
+            5.0, 6.0, 7.0, 7.0, 6.0, 5.0,
+            6.0, 7.0, 8.0, 8.0, 7.0, 6.0].sliced(6,6);
+    ret2 = weighted_restriction!(double, 2)(arr2);
+    correct2 = [[1.0, 3.0, 2.0, 1.0],
+                [3.0, 4.75, 4.0, 3.0],
+                [5.0, 6.75, 6.0, 5.0],
+                [6.0, 8.0, 7.0, 6.0]];
+    assert (ret2 == correct2);
+}
+
+
+// Test weighted_restriction 3D double
+unittest
+{
+    auto arr = iota([5, 5, 5]).as!double.slice;
+    auto ret = weighted_restriction!(double, 3)(arr);
+    auto correct = [[[  0.,   2.,   4.],
+        [ 10.,  12.,  14.],
+        [ 20.,  22.,  24.]],
+
+       [[ 50.,  52.,  54.],
+        [ 60.,  62.,  64.],
+        [ 70.,  72.,  74.]],
+
+       [[100., 102., 104.],
+        [110., 112., 114.],
+        [120., 122., 124.]]];
+    assert (ret == correct);
+
+    arr = iota([6, 6, 6]).as!double.slice;
+    ret = restriction!(double,3)(arr);
+    correct = [[[  0.,   2.,   4.,   5.],
+        [ 12.,  14.,  16.,  17.],
+        [ 24.,  26.,  28.,  29.],
+        [ 30.,  32.,  34.,  35.]],
+
+       [[ 72.,  74.,  76.,  77.],
+        [ 84.,  86.,  88.,  89.],
+        [ 96.,  98., 100., 101.],
+        [102., 104., 106., 107.]],
+
+       [[144., 146., 148., 149.],
+        [156., 158., 160., 161.],
+        [168., 170., 172., 173.],
+        [174., 176., 178., 179.]],
+
+       [[180., 182., 184., 185.],
+        [192., 194., 196., 197.],
+        [204., 206., 208., 209.],
+        [210., 212., 214., 215.]]];
+    assert (ret == correct);
 }
