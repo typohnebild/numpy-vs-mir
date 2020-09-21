e7fa773dc487d29d03d1bb260fd27b0b9a1ae92c
Christoph Alt <christoph.alt@fau.de>
2020-09-21 10:28:21 +0200

    convience function for random matrix generation

--- D/source/multid/tools/util.d
+++ D/source/multid/tools/util.d
@@ -1,4 +1,8 @@
 module multid.tools.util;
+import mir.ndslice : Slice, slice;
+import std.range : generate;
+import std.random : uniform;
+import std.algorithm : fill;
 
 template Timer()
 {
@@ -18,4 +22,12 @@ template Timer()
         sw.stop;
         writeln(text, " ", sw.peek.total!"msecs");
     }
+}
+
+Slice!(T*, Dim) randomMatrix(T, size_t Dim)(size_t N)
+{
+    size_t[Dim] shape = N;
+    auto ret = slice!T(shape);
+    ret.field.fill(generate!(() => uniform(0.0, 1.0)));
+    return ret;
 }
