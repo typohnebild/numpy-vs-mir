8e078ad5ccbd6f833155a46166fc44fd80645c6f
Christoph Alt <christoph.alt@fau.de>
2020-10-19 13:55:25 +0200

    added more smaller problems for gsrb run

--- scripts/masterrun.sh
+++ scripts/masterrun.sh
@@ -5,36 +5,39 @@ buildconf=${2:-'multigrid'}
 
 [ "$buildconf" = "multigrid" ] || [ "$buildconf" = "gsrb" ] || exit 1
 
-generate_problems(){
+generate_problems() {
 	[ -e "$problempath" ] || mkdir -p "$problempath"
 	# delete existing problems
 	rm -f "$problempath/"*.npy
-	STEP=$( [ "$buildconf" = "multigrid" ] && echo "64" || echo "5")
+	STEP=$([ "$buildconf" = "multigrid" ] && echo "64" || echo "5")
 	# generate new problems
-	for i in $(seq 1 20)
-	do
-		../Python/problemgenerator/generate.py "$problempath" 2 $((i*STEP))
+	for i in $(seq 1 20); do
+		../Python/problemgenerator/generate.py "$problempath" 2 $((i * STEP))
 	done
-	if [ "$buildconf" = "multigrid" ]
-	then
+
+	if [ "$buildconf" = "gsrb" ]; then
+		for i in $(seq 1 10); do
+			../Python/problemgenerator/generate.py "$problempath" 2 $((i * 128))
+		done
+	fi
+
+	if [ "$buildconf" = "multigrid" ]; then
 		../Python/problemgenerator/generate.py "$problempath" 2 16
 		../Python/problemgenerator/generate.py "$problempath" 2 32
 		../Python/problemgenerator/generate.py "$problempath" 2 48
 		N=1280
-		for i in $(seq 1 10)
-		do
-			../Python/problemgenerator/generate.py "$problempath" 2 $((N +  i*128))
+		for i in $(seq 1 10); do
+			../Python/problemgenerator/generate.py "$problempath" 2 $((N + i * 128))
 		done
 		N=2560
-		for i in $(seq 1 6)
-		do
-			../Python/problemgenerator/generate.py "$problempath" 2 $((N +  i*256))
+		for i in $(seq 1 6); do
+			../Python/problemgenerator/generate.py "$problempath" 2 $((N + i * 256))
 		done
 	fi
 }
 
 # source of virtual Python environment
-run_virtual(){
+run_virtual() {
 	cd ../Python/ || exit 1
 	. ./venv/bin/activate || exit 1
 	./run.sh "openblas" "$problempath" "${buildconf}"
@@ -42,14 +45,14 @@ run_virtual(){
 }
 
 # source of intel Python environment
-run_intel(){
+run_intel() {
 	cd ../Python/ || exit 1
 	. ./intelpython3/bin/activate || exit 1
 	./run.sh "intel" "$problempath" "${buildconf}"
 	# conda deactivate
 }
 
-run_d(){
+run_d() {
 	./benchmark.sh "$problempath" "$1"
 }
 
@@ -59,8 +62,7 @@ oldpwd=$(pwd)
 
 cd ../D || exit 1
 dub build --force --compiler=ldc --build=release-nobounds --config="$buildconf"
-for x in "field" "naive" "slice"
-do
+for x in "field" "naive" "slice"; do
 	run_d "./$buildconf -s $x"
 done
 cd "$oldpwd" || exit 1
