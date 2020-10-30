66364b12bdc3050f7e27d3c56a0cb3d88992252c
Christoph Alt <christoph.alt@fau.de>
2020-10-30 11:19:58 +0100

    made the stop criteria indepented from the resolution by multipling the the eps with the number of gridpoints

--- D/source/multid/multigrid/multigrid.d
+++ D/source/multid/multigrid/multigrid.d
@@ -11,6 +11,8 @@ Method to run some multigrid steps for abstract cycle
 +/
 Slice!(T*, Dim) multigrid(T, size_t Dim)(Cycle!(T, Dim) cycle, Slice!(T*, Dim) U, size_t iter_cycle, double eps)
 {
+    //scale the epsilon with the number of gridpoints
+    eps *= U.shape[0] * U.shape[0];
     foreach (i; 1 .. iter_cycle + 1)
     {
 
@@ -42,7 +44,7 @@ Returns: U
 +/
 Slice!(T*, Dim) poisson_multigrid(T, size_t Dim, uint v1, uint v2)(
         Slice!(T*, Dim) F, Slice!(T*, Dim) U, uint level, uint mu, size_t iter_cycles,
-        string sweep = "field", T eps = 1e-3, T h = 0)
+        string sweep = "field", T eps = 1e-6, T h = 0)
 {
     Cycle!(T, Dim) cycle;
     switch (sweep)
--- Python/multipy/multigrid/__init__.py
+++ Python/multipy/multigrid/__init__.py
@@ -10,24 +10,27 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def poisson_multigrid(F, U, l, v1, v2, mu, iter_cycle, h=None):
+def poisson_multigrid(F, U, l, v1, v2, mu, iter_cycle, eps=1e-6, h=None):
     """Implementation of MultiGrid iterations
        should solve AU = F
        A is poisson equation
        @param U n x n Matrix
-       @param U n x n Matrix
+       @param F n x n Matrix
        @param v1 Gauss Seidel iterations in pre smoothing
        @param v2 Gauss Seidel iterations in post smoothing
        @param mu iterations for recursive call
        @return x n vector
     """
-    eps = 1e-3
+
     cycle = PoissonCycle(F, v1, v2, mu, l, eps, h)
     return multigrid(cycle, U, eps, iter_cycle)
 
 
 def multigrid(cycle, U, eps, iter_cycle):
-    for i in range(1, iter_cycle + 1) :
+
+    # scale the epsilon with the number of gridpoints
+    eps *= U.shape[0] * U.shape[0]
+    for i in range(1, iter_cycle + 1):
         U = cycle(U)
         norm = cycle.norm(U)
         logger.debug(f"Residual has a L2-Norm of {norm:.4} after {i} MGcycle")
