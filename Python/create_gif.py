9b79b9addb99bc99198b99004028ece928a7bbd3
Tobias Schmidt <tobias.ts.schmidt@fau.de>
2020-10-30 18:58:15 +0100

    updated wave.gif

--- Python/create_gif.py
+++ Python/create_gif.py
@@ -14,7 +14,7 @@ DEFAULT_FILE = '../problems/problem_2D_1024.npy'
 def main():
     U, F = load_problem(DEFAULT_FILE)
     N = U.shape[0]
-    cycle = PoissonCycle(F, 2, 2, 1, 0)
+    cycle = PoissonCycle(F, 1, 0, 1, 0)
     ims = []
     fig = plt.figure()
     ax = fig.gca(projection="3d") 
@@ -22,16 +22,17 @@ def main():
     X,Y = np.meshgrid(x,x)
     for i in range(100):
         #im = plt.imshow(U, cmap='RdBu_r', interpolation='nearest')
-        im = ax.plot_surface(X, Y, U, alpha=0.7, cmap='RdBu_r')
-        t = ax.annotate(i, (0.9, 0.1), xycoords='figure fraction')
+        im = ax.plot_surface(X, Y, U, alpha=0.7, cmap='magma')
+        t = ax.annotate(i, (0.1, 0.1), xycoords='figure fraction')
         norm = cycle.norm(U)
-        n = ax.annotate(norm, (0.1, 0.1), xycoords='figure fraction')
+        n = ax.annotate(norm, (0.7, 0.1), xycoords='figure fraction')
         ims.append([im, t, n])
 
         if norm <= 1e-6 * N * N:
             break
         U = cycle(U)
 
+    fig.colorbar(ims[0][0], shrink=0.5, aspect=10, pad=0.1)
     ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat=True)
     writer = animation.PillowWriter(fps=2)
     ani.save('../graphs/wave.gif', writer=writer)
