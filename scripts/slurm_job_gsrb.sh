45c983929fe5815b02b547c0217db36b0f88c8e8
Christoph Alt <iwia027h@testfront1.rrze.uni-erlangen.de>
2021-01-16 09:23:21 +0100

    added results from hpc machine

--- scripts/slurm_job_gsrb.sh
+++ scripts/slurm_job_gsrb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=8
-## allocate nodes for 6 hours
-#SBATCH --time=01:00:00
+#SBATCH --tasks-per-node=1
+## allocate nodes for 4 hours
+#SBATCH --time=04:00:00
 # job name
 #SBATCH --job-name=gsrb_dlang
 #SBATCH --constraint=hwperf
@@ -12,7 +12,4 @@
 #load required modules (compiler, MPI, ...)
 module load python
 # run
-start_run() {
-	masterrun.sh -i -o -p ../problems -b gsrb
-}
-srun start_run
+srun ./masterrun.sh -i -o -p ~/problems -b gsrb
